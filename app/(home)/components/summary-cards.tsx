2c5a4fd9af2693b660f0ccbfc5765f88ccb6b14e
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-08 12:44:11 -0300

    feat: add transaction pie chart component

--- app/(home)/components/summary-cards.tsx
+++ app/(home)/components/summary-cards.tsx
@@ -1,4 +1,3 @@
-import { db } from "@/app/_lib/prisma";
 import {
   PiggyBankIcon,
   TrendingDownIcon,
@@ -9,45 +8,18 @@ import SummaryCard from "./summary-card";
 
 interface SummaryCardsProps {
   month: string;
+  balance: number;
+  depositTotal: number;
+  investmentsTotal: number;
+  expensesTotal: number;
 }
 
-export default async function SummaryCards({ month }: SummaryCardsProps) {
-  const where = {
-    date: {
-      gte: new Date(`2024-${month}-01`),
-      lt: new Date(`2024-${month}-31`),
-    },
-  };
-
-  const depositTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { ...where, type: "DEPOSIT" },
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
-
-  const investmentsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { ...where, type: "INVESTMENT" },
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
-
-  const expensesTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { ...where, type: "EXPENSE" },
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
-
-  const balance = depositTotal - investmentsTotal - expensesTotal;
-
+export default async function SummaryCards({
+  balance,
+  depositTotal,
+  investmentsTotal,
+  expensesTotal,
+}: SummaryCardsProps) {
   return (
     <div className="space-y-6">
       <SummaryCard
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import { isMatch } from "date-fns";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
+import { getDashboard } from "../_data/get-dashboard";
 import SummaryCards from "./components/summary-cards";
 import TimeSelect from "./components/time-select";
+import { TransactionPieChart } from "./components/transaction-pie-chart";
 
 interface HomeProps {
   searchParams: { month: string };
@@ -22,6 +24,8 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
     redirect("?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <Navbar />
@@ -30,7 +34,18 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionPieChart
+                typesPercentage={dashboard.typePercentage}
+                {...dashboard}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

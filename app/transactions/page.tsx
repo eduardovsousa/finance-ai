5d9f05a13d5b66f7653b583782c445f153b6d6fd
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-08 13:49:19 -0300

    feat: ensure pages are corretly scrolled by the user

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
+import { ScrollArea } from "../_components/ui/scroll-area";
 import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_columns/page";
 
@@ -20,16 +21,18 @@ export default async function Transactions() {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
 
-        <DataTable
-          columns={transactionsColumns}
-          data={JSON.parse(JSON.stringify(transactions))}
-        />
+        <ScrollArea>
+          <DataTable
+            columns={transactionsColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
     </>
   );

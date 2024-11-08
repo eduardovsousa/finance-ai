6bc569a7cc111e25b5b77f8ab48384c594e29826
eduardovsousa <eduardo.varela@outlook.com.br>
2024-11-08 13:41:41 -0300

    feat: add last transactions components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./components/expenses-per-category";
+import LastTransactions from "./components/last-transactions";
 import SummaryCards from "./components/summary-cards";
 import TimeSelect from "./components/time-select";
 import { TransactionPieChart } from "./components/transaction-pie-chart";
@@ -36,7 +37,7 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
           <TimeSelect />
         </div>
 
-        <div className="grid grid-cols-[2fr,1fr]">
+        <div className="grid grid-cols-[2fr,1fr] gap-6">
           <div className="flex flex-col gap-6">
             <SummaryCards month={month} {...dashboard} />
             <div className="grid grid-cols-3 grid-rows-1 gap-6">
@@ -46,6 +47,8 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
               />
             </div>
           </div>
+
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
